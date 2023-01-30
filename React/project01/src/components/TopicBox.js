c0ce566200d9d3d04fb8004d70b717c058a91b48
Lithara <litharaperera2002@gmail.com>
2023-01-30 13:05:30 +0530

    react

--- React/project01/src/components/TopicBox.js
+++ React/project01/src/components/TopicBox.js
@@ -2,10 +2,16 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+    //onst topic="My favourite foods are kottu "
+    //const item="and rice"
+    const a=10;
+    const b=20;
+    const c=a+b;
+
   return (
     <div>
       <div class="topicBox">
-        <span class="text">This is the Topic</span>
+        <span class="text">{c}</span>
       </div>
     </div>
   )
