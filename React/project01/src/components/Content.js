956cc5398b8a932e84662af4d0c8780f2454ae53
Lithara <litharaperera2002@gmail.com>
2023-01-30 12:16:42 +0530

    react

---
+++ React/project01/src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './content.css'
+
+export default function Content() {
+  return (
+    <div class="paragraph">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  )
+}
+
---
+++ React/project01/src/components/TopicBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './topicbox.css'
+
+export default function TopicBox() {
+  return (
+    <div>
+      <div class="topicBox">
+        <span class="text">This is the Topic</span>
+      </div>
+    </div>
+  )
+}
