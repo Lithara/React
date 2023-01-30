6a483a0e4f263cac6a88582f6a669ded4baa0a62
Lithara <litharaperera2002@gmail.com>
2023-01-30 22:31:40 +0530

    react

--- React/project01/src/components/TopicBox.js
+++ React/project01/src/components/TopicBox.js
@@ -7,7 +7,9 @@ export default function TopicBox(props) {
   return (
     <div>
       <div class="topicBox">
-        <span class="text">My favourite food is {props.food}. The price is {props.price}</span>
+        <span class="text">My favourite food is {props.food}. 
+        The price is {props.price}</span>
+        {props.children}
       </div>
     </div>
   )
