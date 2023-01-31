f34256be6ab2081c514b32680564487bb5092540
Lithara <litharaperera2002@gmail.com>
2023-01-31 20:34:17 +0530

    react

--- React/project01/src/App.js
+++ React/project01/src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import { BrowserRouter as Router,Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div>
+        <switch>
+
+          <route path="/">
+            <Home/>
+          </route>
+
+          <route path="/About">
+            <About/>
+          </route>
+
+          <route path="/contact">
+            <Contact/>
+          </route>
+          
+        </switch>
+      </div>
+    </Router>
   );
 }
 
