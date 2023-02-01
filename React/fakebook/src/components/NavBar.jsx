f6991e894ba48c7e3fef43c27bda8d81df2689a3
Lithara <litharaperera2002@gmail.com>
2023-02-01 19:32:44 +0530

    react project 01

--- React/fakebook/src/components/NavBar.jsx
+++ React/fakebook/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import MessageIcon from '@mui/icons-material/Message';
 import SettingsIcon from '@mui/icons-material/Settings';
+import "./NavBar.css"
 
 export default function NavBar() {
   return (
