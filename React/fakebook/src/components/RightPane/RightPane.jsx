3b2fa66842092a5d71993e9814f7a972a17ba37e
Lithara <litharaperera2002@gmail.com>
2023-02-04 22:12:28 +0530

    react project 01

--- React/fakebook/src/components/RightPane/RightPane.jsx
+++ React/fakebook/src/components/RightPane/RightPane.jsx
@@ -3,6 +3,16 @@ import "./RightPane.css"
 
 export default function RightPane() {
   return (
-    <div className="rightPaneBox">RightPane</div>
+    <div className="rightPaneBox">
+      <div className="rightPaneContainer">
+        <div className="adContainer">
+          <span className="sponsored">Sponsored</span>
+          <img src="/images/1.jpeg" alt="" className="adImage" />
+          <span className="adText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
