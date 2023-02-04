dd04148fa828a8fbdbf02f84d4fe4198f0efdbd2
Lithara <litharaperera2002@gmail.com>
2023-02-05 00:01:47 +0530

    react project 01

--- React/fakebook/src/components/RightPane/RightPane.jsx
+++ React/fakebook/src/components/RightPane/RightPane.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./RightPane.css"
+import CakeIcon from '@mui/icons-material/Cake';
 
 export default function RightPane() {
   return (
@@ -11,6 +12,70 @@ export default function RightPane() {
           <span className="adText">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
           </span>
+        </div>
+        <div className="birthdayContainer">
+          <CakeIcon htmlColor="tomato" className="birthdayImage" />
+          <span className="birthdayText">
+            <b>Kasun Gayantha</b> and <b>2 others</b> having birthday today
+          </span>
+        </div>
+        <div className="friendList">
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span classNmae="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span classNmae="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span classNmae="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span classNmae="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span classNmae="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span classNmae="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span classNmae="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/1.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Jennie Sherif</span>
+          </li>
         </div>
       </div>
     </div>
