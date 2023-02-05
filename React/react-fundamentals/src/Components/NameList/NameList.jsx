24eee6ac3fa9899d25cbb84eff6b6796bb7f6691
Lithara <litharaperera2002@gmail.com>
2023-02-06 00:16:57 +0530

    react project 02

--- React/react-fundamentals/src/Components/NameList/NameList.jsx
+++ React/react-fundamentals/src/Components/NameList/NameList.jsx
@@ -7,11 +7,31 @@ export default function NameList() {
       <h1>Name List</h1>
       <hr/>
         <ul>
-          <NameListItem name="Lithara Perera" course="Csharp" email="litharaperera@gmail.com"/>
-          <NameListItem name="Wasath Theekshana" course="React JS" email="wasaththeekshana@gmail.com"/>
-          <NameListItem name="Lorem Ipsum" course="Azure" email="loremipsum@gmail.com"/>
-          <NameListItem name="Fiqri Ismail" course="Software Arcchitecture" email="fiqriismail@gmail.com"/>
-          <NameListItem name="Chamindu Sooriyapperuma" course="Devops" email="chamindusooriyapperuma@gmail.com"/>
+          <NameListItem 
+          name="Lithara Perera" 
+          course="Csharp" 
+          email="litharaperera@gmail.com"
+          />
+          <NameListItem 
+          name="Wasath Theekshana" 
+          course="React JS" 
+          email="wasaththeekshana@gmail.com"
+          />
+          <NameListItem 
+          name="Lorem Ipsum" 
+          course="Azure" 
+          email="loremipsum@gmail.com"
+          />
+          <NameListItem 
+          name="Fiqri Ismail" 
+          course="Software Arcchitecture" 
+          email="fiqriismail@gmail.com"
+          />
+          <NameListItem 
+          name="Chamindu Sooriyapperuma" 
+          course="Devops" 
+          email="chamindusooriyapperuma@gmail.com"
+          />
         </ul>
     </React.Fragment>
   )
