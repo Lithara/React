7e24dc8c1d03cc54677c956bac9db53e06c95731
Lithara <litharaperera2002@gmail.com>
2023-02-06 22:58:46 +0530

    react project 02

--- React/react-fundamentals/src/Components/NameList/NameList.jsx
+++ React/react-fundamentals/src/Components/NameList/NameList.jsx
@@ -3,24 +3,51 @@ import NameListItem from './NameListItem'
 
 export default function NameList() {
 
-  const nameList = {
-    "name": {
-    "title": "Miss",
-    "first": "Jennie",
-    "last": "Nichols"
+  const nameList = [
+   { 
+    name : { title: 'mr', first: 'rafael', last: 'leroux' },
+    location : { city: 'madrid' },
+    email: 'rafael.leroux@example.com',
+    dob: { date : '1988-11-12T06:26:38.812Z', age : 32 },
+    picture: { medium: 'https://randomuser.me/api/portraits/med/men/75.jpg' },
+  },
+  {
+    name : {
+    title : 'miss',
+    first : 'jennie',
+    last : 'nichols'
     },
-    "location": {
-      "city": "Billings"
+    location : {
+      city: 'billings'
     },
-    "email": "jennie.nichols@example.com",
-    "dob": {
-      "date": "1992-03-08T15:13:16.688Z",
-      "age": 30
+    email : 'jennie.nichols@example.com',
+    dob : {
+      date : '1992-03-08T15:13:16.688Z',
+      age : 30
     },
-    "picture": {
-      "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+    picture : {
+      medium : 'https://randomuser.me/api/portraits/med/men/78.jpg',
     },
-  };
+  },
+  {
+    name : {
+    title : 'mr',
+    first : 'bred',
+    last : 'gibson'
+    },
+    location : {
+      city : 'kilcoole'
+    },
+    email : 'bred.gibson@example.com',
+    dob : {
+      date : '1993-07-20T09:44:18.674Z',
+      age : 26
+    },
+    picture : {
+      medium : 'https://randomuser.me/api/portraits/med/men/8.jpg',
+    },
+  },
+];
 
   return (
     <React.Fragment>
@@ -28,11 +55,25 @@ export default function NameList() {
       <hr/>
         <ul>
           <NameListItem 
-          name={nameList.name.first + ' ' + nameList.name.last} 
-          city={nameList.location.city}
-          email={nameList.email}
-          birthday={nameList.dob.date}
-          avatar={nameList.picture.medium}
+          name={nameList[0].name.first + ' ' + nameList.name.last} 
+          city={nameList[0].location.city}
+          email={nameList[0].email}
+          birthday={nameList[0].dob.date}
+          avatar={nameList[0].picture.medium}
+          />
+          <NameListItem 
+          name={nameList[1].name.first + ' ' + nameList.name.last} 
+          city={nameList[1].location.city}
+          email={nameList[1].email}
+          birthday={nameList[1].dob.date}
+          avatar={nameList[1].picture.medium}
+          />
+          <NameListItem 
+          name={nameList[2].name.first + ' ' + nameList.name.last} 
+          city={nameList[2].location.city}
+          email={nameList[2].email}
+          birthday={nameList[2].dob.date}
+          avatar={nameList[2].picture.medium}
           />
         </ul>
     </React.Fragment>
