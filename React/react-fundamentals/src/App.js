18ce56ba3a6265f72adafc5b1e4e7e63ca8130e7
Lithara <litharaperera2002@gmail.com>
2023-02-07 18:58:03 +0530

    react project 02

--- React/react-fundamentals/src/App.js
+++ React/react-fundamentals/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import NameList from './Components/NameList/NameList';
+import HeaderBar from './Components/HeaderBar/HeaderBar'
 
 function App() {
   return (
     <div>
+        <HeaderBar/>
         <NameList/>
     </div>
   );
