3ec4cc67b1da2e2d7a35fbde7ed897a37467f12a
Lithara <litharaperera2002@gmail.com>
2023-02-09 23:59:57 +0530

    react project 02

---
+++ GitHub/React/react-fundamentals/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NameList from './Components/NameList/NameList';
+import HeaderBar from './Components/HeaderBar/HeaderBar'
+
+function App() {
+  return (
+    <div>
+        <HeaderBar/>
+        <NameList/>
+    </div>
+  );
+}
+
+export default App;
