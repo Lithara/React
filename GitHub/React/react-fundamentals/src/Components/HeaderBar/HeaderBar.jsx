6d7f2c7bf138c4d0e905bef1ebfe23644d5a7618
Lithara <litharaperera2002@gmail.com>
2023-02-10 12:07:39 +0530

    react project 02

--- GitHub/React/react-fundamentals/src/Components/HeaderBar/HeaderBar.jsx
+++ GitHub/React/react-fundamentals/src/Components/HeaderBar/HeaderBar.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function HeaderBar() {
   return (
-    <nav className="navBar navBar-light bg-light shadow-sm">
+    <nav className="navBar navBar-expand-lg navBar-light bg-light shadow-sm">
         <span className="navBar-brand mb-0 h1">Name List</span>
+        <ul classNmae="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Show Name List
+            </a>
+          </li>
+        </ul>
     </nav>
-  )
+  );
 }
