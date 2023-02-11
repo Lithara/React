dd97c79fd20ae38c9ce2d0c536fc9cd0ea67ebb0
Lithara <litharaperera2002@gmail.com>
2023-02-11 12:34:23 +0530

    react project 02

---
+++ GitHub/React/react-fundamentals/src/Components/Pages/About/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div className="container mt-4">
+        <h1>About</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna 
+        aliqua. 
+        </p>
+    </div>
+    </>
+  );
+}
